20978ffe0d5c7ad27d78950e0d68cc9416f59583
graziellicosta <grazielli.silva.costa@escola.pr.gov.br>
2025-06-12 11:21:55 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
 
-function setup() {
-  createCanvas(800, 800);
-  fill("#31A4D9");
-}
-
-function draw() {
-  background(800);
-} 
 let campo = [];
 let cols = 5;
 let rows = 3;
 let tamanho = 80;
 let tempocresCimento = 5000;// 5 segundos pra crescer
 let produtosColhidos = 0;
+function  setup() {
+  createCanvas(600,400);
+  for(let i = 0; i < cols * rows; i++) {
+      campo.push({
+        estado: "vazio",// "vazio", "plantado", "regado", "maduro"
+      temPlantio: 0 
+   });  
+  }  
+  function colher(){
+    console.log("Botão colher foi clicado");
+  }
+  createButton("colher").position(420, 50).mousePressed(colher);
+  function transportar() {
+    console.log("Botão transportar foi clicado");
+  }
+  createButton("transportar").position(420,90).mousePressed(transportar);
+}
+function draw(){
+  background(180,230,180);
+  drawCampo();
+  mostrarinfos();
+  atualizarPlantas();
+}
