26dcd979ef6c83abc80315733991a06c28efd047
graziellicosta <grazielli.silva.costa@escola.pr.gov.br>
2025-06-12 10:39:34 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  fill("#31A4D9");
 }
 
 function draw() {
-  background(220);
-}
+  background(800);
+} 
+let campo = [];
+let cols = 5;
+let rows = 3;
+let tamanho = 80;
+let tempocresCimento = 5000;// 5 segundos pra crescer
+let produtosColhidos = 0;
